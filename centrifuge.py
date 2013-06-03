afef18b9605501d864d8c0b2bd13cf20ff8f4bcf
Zach <zach@stamen.com>
2013-06-03 23:54:50 +0000

    added HSV adjustments

--- centrifuge.py
+++ centrifuge.py
@@ -4,6 +4,7 @@ import tornado.web
 import tornado.httpclient
 import Image
 import numpy
+import numpy as np
 from numpy import fromstring, ubyte, e, array, add, uint8, copy, clip
 from scipy.ndimage.filters import convolve1d
 from cStringIO import StringIO
@@ -22,8 +23,143 @@ filters = {
     "gaussian": {"needs_border": True},
     "invert": {"needs_border": False},
     "contrast": {"needs_border": False},
+    "hsv": {"needs_border": False},
     "levels": {"needs_border": False}
 }
+
+def rgb2hsv(rgb):
+    """RGB to HSV color space conversion.
+
+    Parameters
+    ----------
+    rgb : array_like
+        The image in RGB format, in a 3-D array of shape (.., .., 3).
+
+    Returns
+    -------
+    out : ndarray
+        The image in HSV format, in a 3-D array of shape (.., .., 3).
+
+    Raises
+    ------
+    ValueError
+        If `rgb` is not a 3-D array of shape (.., .., 3).
+
+    Notes
+    -----
+    The conversion assumes an input data range of [0, 1] for all
+    color components.
+
+    Conversion between RGB and HSV color spaces results in some loss of
+    precision, due to integer arithmetic and rounding [1]_.
+
+    References
+    ----------
+    .. [1] http://en.wikipedia.org/wiki/HSL_and_HSV
+
+    Examples
+    --------
+    >>> from skimage import color
+    >>> from skimage import data
+    >>> lena = data.lena()
+    >>> lena_hsv = color.rgb2hsv(lena)
+    """
+    arr = rgb#_prepare_colorarray(rgb)
+    out = np.empty_like(arr)
+
+    # -- V channel
+    out_v = arr.max(-1)
+
+    # -- S channel
+    delta = arr.ptp(-1)
+    # Ignore warning for zero divided by zero
+    old_settings = np.seterr(invalid='ignore')
+    out_s = delta / out_v
+    out_s[delta == 0.] = 0.
+
+    # -- H channel
+    # red is max
+    idx = (arr[:, :, 0] == out_v)
+    out[idx, 0] = (arr[idx, 1] - arr[idx, 2]) / delta[idx]
+
+    # green is max
+    idx = (arr[:, :, 1] == out_v)
+    out[idx, 0] = 2. + (arr[idx, 2] - arr[idx, 0]) / delta[idx]
+
+    # blue is max
+    idx = (arr[:, :, 2] == out_v)
+    out[idx, 0] = 4. + (arr[idx, 0] - arr[idx, 1]) / delta[idx]
+    out_h = (out[:, :, 0] / 6.) % 1.
+    out_h[delta == 0.] = 0.
+
+    np.seterr(**old_settings)
+
+    # -- output
+    out[:, :, 0] = out_h
+    out[:, :, 1] = out_s
+    out[:, :, 2] = out_v
+
+    # remove NaN
+    out[np.isnan(out)] = 0
+
+    return out
+
+
+def hsv2rgb(hsv):
+    """HSV to RGB color space conversion.
+
+    Parameters
+    ----------
+    hsv : array_like
+        The image in HSV format, in a 3-D array of shape (.., .., 3).
+
+    Returns
+    -------
+    out : ndarray
+        The image in RGB format, in a 3-D array of shape (.., .., 3).
+
+    Raises
+    ------
+    ValueError
+        If `hsv` is not a 3-D array of shape (.., .., 3).
+
+    Notes
+    -----
+    The conversion assumes an input data range of [0, 1] for all
+    color components.
+
+    Conversion between RGB and HSV color spaces results in some loss of
+    precision, due to integer arithmetic and rounding [1]_.
+
+    References
+    ----------
+    .. [1] http://en.wikipedia.org/wiki/HSL_and_HSV
+
+    Examples
+    --------
+    >>> from skimage import data
+    >>> lena = data.lena()
+    >>> lena_hsv = rgb2hsv(lena)
+    >>> lena_rgb = hsv2rgb(lena_hsv)
+    """
+    arr = hsv#_prepare_colorarray(hsv)
+
+    hi = np.floor(arr[:, :, 0] * 6)
+    f = arr[:, :, 0] * 6 - hi
+    p = arr[:, :, 2] * (1 - arr[:, :, 1])
+    q = arr[:, :, 2] * (1 - f * arr[:, :, 1])
+    t = arr[:, :, 2] * (1 - (1 - f) * arr[:, :, 1])
+    v = arr[:, :, 2]
+
+    hi = np.dstack([hi, hi, hi]).astype(np.uint8) % 6
+    out = np.choose(hi, [np.dstack((v, t, p)),
+                         np.dstack((q, v, p)),
+                         np.dstack((p, v, t)),
+                         np.dstack((p, q, v)),
+                         np.dstack((t, p, v)),
+                         np.dstack((v, p, q))])
+
+    return out
 
 class MainHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
@@ -105,6 +241,8 @@ class MainHandler(tornado.web.RequestHandler):
 
         elif self.__filter == "contrast":
             tile = self.contrast(metatile, self.__args["percent"])
+        elif self.__filter == "hsv":
+            tile = self.hsv(metatile, self.__args)
         elif self.__filter == "levels":
             tile = self.levels(metatile, self.__args)
         elif self.__filter == "invert":
@@ -155,9 +293,6 @@ class MainHandler(tornado.web.RequestHandler):
         arr = numpy.array(image)
         return Image.fromarray(arr)
 
-    def rgbToHSV(self, arr):
-        chroma = 5
-
     def levels(self, image, args):
         in_min, gamma, in_max, out_min, out_max = float(args["in_min"]), float(args["gamma"]), float(args["in_max"]), float(args["out_min"]), float(args["out_max"])
         arr = numpy.array(image)
@@ -165,6 +300,23 @@ class MainHandler(tornado.web.RequestHandler):
         arr_gamma = numpy.power(arr_normal, gamma)
         arr_rescale = numpy.clip(arr_gamma * (out_max - out_min) + out_min, 0.0, 255.0)
         arr[...,0:3] = arr_rescale
+        return Image.fromarray(arr)
+
+    def hsv(self, image, url_args):
+        args = {
+            "hue": url_args.get("hue", 0.0) / 360.0,
+            "saturation": url_args.get("saturation", 0.0) / 255.0,
+            "value": url_args.get("value", 0.0) / 255.0
+        }
+        print "HSV"
+        arr = numpy.array(image)
+        rgb = arr[...,0:3] / 255.0
+        hsv = rgb2hsv(rgb)
+        hsv[...,0] += float(args["hue"])
+        hsv[...,0] = hsv[...,0] % 1.0
+        hsv[...,1] = numpy.clip(hsv[...,1] + float(args["saturation"]), 0.0, 1.0)
+        hsv[...,2] = numpy.clip(hsv[...,2] + float(args["value"]), 0.0, 1.0)
+        arr[...,0:3] = hsv2rgb(hsv) * 255.0
         return Image.fromarray(arr)
 
 application = tornado.web.Application([
